1117aa95b5cc86e45ad85a955f67bfc51f4cfaa5
Matheus Oliveira <matheus.adeoliveira11@gmail.com>
2026-03-13 15:22:47 -0300

    fix: extends prisma client providing adapter

--- src/shared/database/services/prisma.service.ts
+++ src/shared/database/services/prisma.service.ts
@@ -1,5 +1,20 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaClient } from "@prisma/client/extension";
+import { Injectable, OnModuleInit } from "@nestjs/common";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "generated/prisma/client";
 
 @Injectable()
-export class PrismaService extends PrismaClient {}
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    super({
+      adapter,
+    });
+  }
+
+  public async onModuleInit() {
+    await this.$connect();
+  }
+}
