36f37a43d015e161c4934387eb0735a8ff6b10fc
Matheus Oliveira <matheus.adeoliveira11@gmail.com>
2026-03-13 16:14:34 -0300

    fix: remove role guard

--- src/modules/auth/controllers/login.controller.ts
+++ src/modules/auth/controllers/login.controller.ts
@@ -1,14 +1,12 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
-import { Public, RequireRoles } from "src/shared/decorators";
+import { Public } from "src/shared/decorators";
 import { HttpResponse } from "src/shared/types";
 import { LoginService } from "../services/login.service";
 import { LoginRequestDTO } from "../dto/request/login-request.dto";
 import { LoginOutputDTO } from "../dto/io/login-io.dto";
-import { UserTypesEnum } from "src/shared/enum";
 
 @Controller()
 @Public()
-@RequireRoles(UserTypesEnum.BARBER, UserTypesEnum.CUSTOMER)
 export class LoginController {
   constructor(private service: LoginService) {}
 
